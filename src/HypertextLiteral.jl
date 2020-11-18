c7022899a2375c27ae3ee94b786ae40b74f0c555
Clark C. Evans <cce@clarkevans.com>
2020-11-18 15:10:01 -0600

    much of this complexity seems unnecessary

--- src/HypertextLiteral.jl
+++ src/HypertextLiteral.jl
@@ -12,10 +12,10 @@ macro htl_str(expr::String)
     if !occursin("\$", expr)
         return Expr(:call, :HTML, expr)
     end
-    return Expr(:call, :HTML, htl_str(expr, :content, Symbol[]))
+    return Expr(:call, :HTML, htl_str(expr, :content))
 end
 
-function htl_str(expr::String, cntx::Symbol, locals::Vector{Symbol})::Expr
+function htl_str(expr::String, cntx::Symbol)::Expr
     start = 1
     mixed = false
     args = []
@@ -46,14 +46,7 @@ function htl_str(expr::String, cntx::Symbol, locals::Vector{Symbol})::Expr
             continue
         end
         mixed = true
-        if isa(nest, Symbol)
-            if !in(nest, locals)
-                nest = esc(nest)
-            end
-            push!(args, Expr(:call, :htl_escape, nest))
-            continue
-        end
-        push!(args, htl_str(nest, cntx, locals))
+        push!(args, Expr(:call, :htl_escape, esc(nest)))
     end
     if mixed
         return Expr(:call, :string, args...)
@@ -61,43 +54,11 @@ function htl_str(expr::String, cntx::Symbol, locals::Vector{Symbol})::Expr
     return Expr(:call, :string, join(args))
 end
 
-function htl_str(expr::Expr, cntx::Symbol, locals::Vector{Symbol})::Expr
-
-    #  htl"""<ul>$(map([1,2]) do x htl"<li>$x</li>" end)</ul>"""
-    if expr.head == :do
-       @assert expr.args[2].head == Symbol("->")
-       scope = expr.args[2].args[1]
-       @assert scope.head == :tuple
-       @assert length(scope.args) == 1
-       nested_locals = [scope.args[1], locals...]
-       block = expr.args[2].args[2]
-       @assert block.head == :block
-       @assert length(block.args) == 2
-       block.args[2] = htl_str(block.args[2], cntx, nested_locals)
-       return Expr(:call, :join, expr)
-    end
-
-    # TODO: improve translation to handle local variables
-    if expr.head == :call && length(locals) == 0
-        return Expr(:call, :htl_escape, esc(expr))
-    end
-
-    if expr.head == :macrocall && expr.args[1] == Symbol("@htl_str")
-        return htl_str(expr.args[3], cntx, locals)
-    end
-
-    # so this is basically when we have failed to parse...
-    if expr.head == :incomplete
-        throw(ErrorException("unable to handle escape sequences"))
-    end
-
-    # unless it is well defined, let's not translate it
-    throw(DomainError(expr, "undefined interpolation"))
-end
-
 function htl_escape(var, ctx::Symbol=:content)::String
     if isa(var, HTML{String})
         return var.content
+    elseif isa(var, Vector{HTML{String}})
+        return join([part.content for part in var])
     elseif isa(var, AbstractString)
         return replace(replace(var, "&" => "&amp;"), "<" => "&lt;")
     elseif isa(var, Number)

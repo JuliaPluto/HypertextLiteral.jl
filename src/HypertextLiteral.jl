7ad4d61b80f44cc2ce10691556625fbd06e97957
Clark C. Evans <cce@clarkevans.com>
2020-11-25 18:47:53 -0600

    adding docstrings for htl_escape and @htl_str

--- src/HypertextLiteral.jl
+++ src/HypertextLiteral.jl
@@ -3,20 +3,19 @@ module HypertextLiteral
 export @htl_str, htl_escape
 
 """
-    @htl_str -> Docs.HTML
+    @htl_str -> Base.Docs.HTML{String}
 
-Create an `HTML` object with string interpolation. The dollar-sign
-character may be escaped by doubling it.
+Create a `HTML{String}` with string interpolation (`\$`) that uses
+context-sensitive hypertext escaping. Escaping of interpolated results
+is performed by `htl_escape`. Escape sequences should work identically
+to Julia strings, except in cases where a slash immediately precedes the
+double quote (see `@raw_str` and Julia issue #22926 for details).
 """
 macro htl_str(expr::String)
     if !occursin("\$", expr)
         return Expr(:call, :HTML, unescape_string(expr))
     end
-    return Expr(:call, :HTML, htl_str(expr, :content))
-end
-
-function htl_str(expr::AbstractString, cntx::Symbol)::Expr
-    # TODO: track hypertext context for proper escaping
+    cntx = :content  # this is the escaping context
     args = Union{String, Expr}[]
     svec = String[]
     start = idx = 1
@@ -65,33 +64,48 @@ function htl_str(expr::AbstractString, cntx::Symbol)::Expr
         push!(args, join(svec))
         empty!(svec)
     end
-    return Expr(:call, :string, args...)
+    return Expr(:call, :HTML, Expr(:call, :string, args...))
 end
 
-function htl_escape(ctx::Symbol, var)::String
-    # TODO: take hypertext context into account while escaping
-    if isa(var, HTML{String})
-        return var.content
-    elseif isa(var, Vector{HTML{String}})
-        return join([part.content for part in var])
-    elseif isa(var, AbstractString)
-        return replace(replace(var, "&" => "&amp;"), "<" => "&lt;")
-    elseif isa(var, Number)
-        return string(var)
+"""
+    htl_escape(context::Symbol, obj)::String
+
+For a given HTML lexical context and an arbitrary Julia object, return
+a `String` value that is properly escaped. Splatting interpolation
+concatinates these escaped values. This fallback implements:
+`HTML{String}` objects are assumed to be propertly escaped, and hence
+its content is returned; `Vector{HTML{String}}` are concatinated; any
+`Number` is converted to a string using `string()`; and `AbstractString`
+objects are escaped according to context.
+
+There are several escaping contexts. The `:content` context is for HTML
+content, at a minimum, the amperstand (`&`) and lessthan (`<`) characters
+must be escaped.
+"""
+function htl_escape(context::Symbol, obj)::String
+    @assert context == :content
+    if isa(obj, HTML{String})
+        return obj.content
+    elseif isa(obj, Vector{HTML{String}})
+        return join([part.content for part in obj])
+    elseif isa(obj, AbstractString)
+        return replace(replace(obj, "&" => "&amp;"), "<" => "&lt;")
+    elseif isa(obj, Number)
+        return string(obj)
     else
         extra = ""
-        if isa(var, AbstractVector)
+        if isa(obj, AbstractVector)
             extra = ("\nPerhaps use splatting? e.g. " *
                      "htl\"\"\"\$([x for x in 1:3]...)\"\"\"")
         end
-        throw(DomainError(var,
-         "Type $(typeof(var)) lacks an `htl_escape` specialization.$(extra)"))
+        throw(DomainError(obj,
+         "Type $(typeof(obj)) lacks an `htl_escape` specialization.$(extra)"))
     end
 end
 
-function htl_escape(ctx::Symbol, var...)::String
-    # htl"""$([x for x in [1:3]]...)"""
-    return join([htl_escape(ctx, x) for x in var])
+function htl_escape(context::Symbol, obj...)::String
+    # support splatting interpolation via concatination
+    return join([htl_escape(context, x) for x in obj])
 end
 
 

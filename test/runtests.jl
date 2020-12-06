a04f64f307834328e19a9c11637dbbb9159c1eea
Clark C. Evans <cce@clarkevans.com>
2020-12-05 18:21:32 -0600

    actions: rename doc directory to docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,5 +20,5 @@ push!(subs, r"\r\n" => "\n")
 ENV["COLUMNS"] = "72"
 
 package_path(x) = relpath(joinpath(dirname(abspath(PROGRAM_FILE)), "..", x))
-default = package_path.(["README.md", "doc/src"])
+default = package_path.(["README.md", "docs/src"])
 runtests(; default=default, subs=subs)

68bce772bec710973fb3fe3a4cf0bf715fa638b4
Clark C. Evans <cce@clarkevans.com>
2020-12-09 06:36:01 -0600

    adding prototype of potential speedup solution

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -65,6 +65,9 @@ htl_test() = begin
    return io
 end
 
+ee(x) = replace(replace(x, "&" => "&amp;"), "<" => "&lt;")
+ea(x) = replace(replace(x, "&" => "&amp;"), "\"" => "&quot;")
+
 reg_database(d) = """
   <html>
     <head><title>Customers & Employees</title></head>
@@ -76,9 +79,9 @@ reg_database(d) = """
 
 reg_customer(c) = """
     <dl>
-      <dt>Company<dd>$(c.company)
-      <dt>Phrase<dd>$(c.phrase)
-      <dt>Active Since<dd>$(c.active)   
+      <dt>Company<dd>$(ee(c.company))
+      <dt>Phrase<dd>$(ee(c.phrase))
+      <dt>Active Since<dd>$(ee(c.active))
       <dt>Employees<dd>
         <table>
           <tr><th>Last Name<th>First Name<th>Title
@@ -90,10 +93,10 @@ reg_customer(c) = """
 """
 
 reg_employee(e) = """
-      <tr><td>$(e.last_name)<td>$(e.first_name)<td>$(e.title)
-          <td><a href="mailto:$(e.email)">$(e.email)</a>
-          <td>$(e.main_number)<td>$(e.cell_phone)
-          <td>$(join(["<span>$c</span>" for c in e.comments]))
+      <tr><td>$(ee(e.last_name))<td>$(ee(e.first_name))<td>$(e.title)
+          <td><a href="mailto:$(ea(e.email))">$(ee(e.email))</a>
+          <td>$(ee(e.main_number))<td>$(ee(e.cell_phone))
+          <td>$(join(["<span>$(ee(c))</span>" for c in e.comments]))
 """
 
 reg_test() = begin
@@ -132,6 +135,75 @@ hs_test() = begin
    return io
 end
 
-println("Hyperscript: ", @benchmark hs_test())
+function H(xs...)
+    HTML() do io
+        for x in xs
+            show(io, MIME"text/html"(), x)
+        end
+    end
+end
+
+function entity(str::AbstractString)
+    @assert length(str) == 1
+    entity(str[1])
+end
+
+entity(ch::Char) = "&#$(Int(ch));"
+
+HE(x) = HTML(replace(x, r"[<&]" => entity))
+HA(x) = HTML(replace(x, r"[<\"]" => entity))
+
+#HE(x) = HTML(replace(replace(x, "&" => "&amp;"), "<" => "&lt;"))
+#HA(x) = HTML(replace(replace(x, "&" => "&amp;"), "\"" => "&quot;"))
+
+new_database(d) =
+   H(HTML("<html><head><title>"), HE("Customers & Employees"),
+     HTML("</title></head><body>"),
+      [new_customer(c) for c in d]...,
+      HTML("</body></html>"))
+
+new_customer(c) =
+   H(HTML("<dl><dt>Company<dd>"), HE(c.company), 
+     HTML("<dt>Phrase<dd>"), HE(c.phrase),
+     HTML("<dt>Active Siince<dd>"), HE(c.active),
+     HTML("""
+      <dt>Employees<dd>
+        <table>
+          <tr><th>Last Name<th>First Name<th>Title
+              <th>E-Mail<th>Office Phone<th>Cell Phone
+              <th>Comments</tr>"""),
+     [new_employee(e) for e in c.employees]...,
+     HTML("</table></dd></dl>"))
+
+new_employee(e) = 
+   H(HTML("<tr><td>"), HE(e.last_name), 
+         HTML("<td>"), HE(e.first_name),
+         HTML("<td>"), HE(e.title),
+         HTML("<td><a href=\"mailto:"), HA(e.email), 
+                    HTML("\">"), HE(e.email), HTML("</a>"),
+         HTML("<td>"), HE(e.main_number), 
+         HTML("<td>"), HE(e.cell_phone),
+         HTML("<td>"),
+          [H(HTML("<span>"), HE(c), HTML("</span>")) for c in e.comments]...)
+
+new_test() = begin
+   io = IOBuffer()
+   ob = new_database(database)
+   show(io, MIME("text/html"), ob)
+   return io
+end
+
 println("interpolate: ", @benchmark reg_test())
+println("Hyperscript: ", @benchmark hs_test())
 println("HypertextLiteral: ", @benchmark htl_test())
+println("New HTML: ", @benchmark new_test())
+
+if false
+    start = time()
+    open("new.html", "w") do f
+       ob = new_database(database)
+       show(f, MIME("text/html"), ob)
+    end
+    finish = time()
+    println("printing ", finish - start)
+end

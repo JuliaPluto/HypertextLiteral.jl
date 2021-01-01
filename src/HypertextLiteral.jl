097e763043000db7d9082a7c2d9cd27bd2092f85
Clark C. Evans <cce@clarkevans.com>
2021-01-01 14:58:46 -0600

    pull out notation non-standard string literal into its own source file

--- src/HypertextLiteral.jl
+++ src/HypertextLiteral.jl
@@ -1,23 +1,20 @@
 """
     HypertextLiteral
 
-This library provides for a `@htl()` macro and a `htl` string literal,
-both implementing interpolation that is aware of hypertext escape
-context. The `@htl` macro has the advantage of using Julia's native
-string parsing, so that it can handle arbitrarily deep nesting. However,
-it is a more verbose than the `htl` string literal and doesn't permit
-interpolated string literals. Conversely, the `htl` string literal,
-`@htl_str`, uses custom parsing letting it handle string literal
-escaping, however, it can only be used two levels deep (using three
-quotes for the outer nesting, and a single double quote for the inner).
+This library provides a `@htl()` macro and a `@htl_str` non-standard
+string literal, both implementing interpolation that is aware of
+hypertext escape context. The `@htl` macro has the advantage of using
+Julia's native string parsing and has familar look and feel. Conversely,
+the `htl` literal, `@htl_str`, takes a less standard approach.
 """
 module HypertextLiteral
 export @htl_str, @htl
 
-include("primitives.jl")    # Wrap, Unwrap, EscapeProxy
-include("macros.jl")   # @htl and @htl_str
-include("convert.jl")  # runtime conversion of objects
-include("lexer.jl")    # interpolate string to macro expression
-include("rewrite.jl")  # macro optimizations called by interpolate
+include("primitives.jl") # Wrap, Unwrap, EscapeProxy
+include("macro.jl")      # @htl macro and `Result` object
+include("notation.jl")   # @htl_str non-standard string literal
+include("convert.jl")    # runtime conversion of objects
+include("lexer.jl")      # interpolate string to macro expression
+include("rewrite.jl")    # macro optimizations called by interpolate
 
 end
---
+++ src/macro.jl
@@ -0,0 +1,65 @@
+"""
+    @htl string-expression
+
+Create a `Result` object with string interpolation (`\$`) that uses
+context-sensitive hypertext escaping. Before Julia 1.6, interpolated
+string literals, e.g. `\$("Strunk & White")`, are treated as errors
+since they cannot be reliably detected (see Julia issue #38501).
+"""
+macro htl(expr)
+    this = Expr(:macrocall, Symbol("@htl"), nothing, expr)
+    if !Meta.isexpr(expr, :string)
+        return interpolate([expr], this)
+    end
+    args = expr.args
+    if length(args) == 0
+        return interpolate([], this)
+    end
+    for part in expr.args
+        if Meta.isexpr(part, :(=))
+            throw(DomainError(part,
+             "assignments are not permitted in an interpolation"))
+        end
+    end
+    if VERSION < v"1.6.0-DEV"
+        # Find cases where we may have an interpolated string literal and
+        # raise an exception (till Julia issue #38501 is addressed)
+        if length(args) == 1 && args[1] isa String
+            throw("interpolated string literals are not supported")
+        end
+        for idx in 2:length(args)
+            if args[idx] isa String && args[idx-1] isa String
+                throw("interpolated string literals are not supported")
+            end
+        end
+    end
+    return interpolate(expr.args, this)
+end
+
+"""
+    Result(expr, unwrap)
+
+Address display modalities by showing the macro expression that
+generated the results when shown on the REPL. However, when used with
+`print()` show the results. This object is also showable to any IO
+stream via `"text/html"`.
+"""
+struct Result
+    content::Function
+    expr::Expr
+end
+
+function Result(expr::Expr, xs...)
+    Result(expr) do io::IO
+        for x in xs
+            print(io, x)
+        end
+    end
+end
+
+Base.show(io::IO, m::MIME"text/html", h::Result) = h.content(EscapeProxy(io))
+Base.print(io::IO, h::Result) = h.content(EscapeProxy(io))
+Base.show(io::IO, h::Result) = print(io, h.expr)
+# avoid a show() dispatch for nested results
+Base.print(io::EscapeProxy, h::Result) = h.content(io)
+content(h::Result) = h

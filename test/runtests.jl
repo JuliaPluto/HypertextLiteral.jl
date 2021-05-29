4c79fe88a890abfe7eb15b75b46a6303eda5247b
Clark C. Evans <cce@clarkevans.com>
2021-05-29 13:01:34 -0500

    upgrade NarrativeTest to integrate possibility of using DocTests as well

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env julia
 
+using Documenter, Logging, NarrativeTest, Test
 using HypertextLiteral
-using NarrativeTest
 
 subs = NarrativeTest.common_subs()
 
@@ -44,4 +44,32 @@ ENV["COLUMNS"] = "72"
 
 package_path(x) = relpath(joinpath(dirname(abspath(PROGRAM_FILE)), "..", x))
 default = package_path.(["README.md", "docs/src"])
-runtests(; default=default, subs=subs)
+
+if isempty(ARGS)
+
+    @testset "HypertextLiteral" begin
+
+    @info "Running doctests..."
+    DocMeta.setdocmeta!(
+        HypertextLiteral,
+        :DocTestSetup,
+        quote
+            using HypertextLiteral: 
+                @htl, @htl_str
+                Reprint, Render, Bypass, EscapeProxy,
+                attribute_value, content, attribute_pair, 
+                inside_tag, rawtext
+            using Dates
+        end)
+    with_logger(Logging.ConsoleLogger(stderr, Logging.Warn)) do
+        doctest(HypertextLiteral)
+    end
+
+    @info "Running narrative tests..."
+    NarrativeTest.testset(; default=default, subs=subs)
+
+    end
+
+else
+    NarrativeTest.testset(ARGS; subs=subs)
+end
